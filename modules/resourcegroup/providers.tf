0dafd9881f13f498d85340e48f09b9e73e6fa39c
namannihal <70774052+namannihal@users.noreply.github.com>
2025-04-18 14:29:38 +0530

    Update providers.tf

--- modules/resourcegroup/providers.tf
+++ modules/resourcegroup/providers.tf
@@ -1,20 +1,9 @@
 terraform {
-  required_version = ">= 1.3.0"
-
+  required_version = ">= 1.9, < 2.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = ">= 3.117, < 5.0"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
-
-  use_oidc        = true # Required for OIDC authentication
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
 }
