11fc2aae99a7c6a16faefc900e0589ab94fb4685
namannihal <70774052+namannihal@users.noreply.github.com>
2025-04-21 11:24:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "location" {
   description = "The Azure region where resources will be deployed"
   type        = string
-  defaut      = East US
+  default      = East US
 }
  
 variable "rg_name" {
   description = "The name of the resource group where resources will be deployed"
   type        = string
-  default     = shared-rg
+  default     = "shared-rg"
 }
  
