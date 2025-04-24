0b3b97832bfdf08c307b52ead5985f482feac847
shubhi0302 <163977746+shubhi0302@users.noreply.github.com>
2025-04-24 14:01:39 +0530

    Update providers.tf

--- modules/resourcegroup/providers.tf
+++ modules/resourcegroup/providers.tf
@@ -1,9 +1,24 @@
 terraform {
   required_version = ">= 1.9, < 2.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.117, < 5.0"
     }
   }
+
+  backend "azurerm" {
+    use_oidc         = true
+    use_azuread_auth = true
+    # Note: All other values like tenant_id, client_id, etc. will be passed via -backend-config during init
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  use_oidc        = true
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
