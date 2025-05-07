1cc1abb708e08c020de5bde48be7ee1b8d07b3be
shubhi0302 <163977746+shubhi0302@users.noreply.github.com>
2025-05-07 11:02:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,21 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "testing-2"
+}
+
 variable "location" {
-  description = "The Azure region where resources will be deployed"
+  description = "Azure region for the Resource Group"
   type        = string
-  default      = "East US"
+  default     = "East US"
 }
- 
-variable "rg_name" {
-  description = "The name of the resource group where resources will be deployed"
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
   type        = string
-  default     = "shared-rg"
 }
- 
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
