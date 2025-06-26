35403db4eaaf6e5153a95671ffd4a05de21fd767
shubhi0302 <163977746+shubhi0302@users.noreply.github.com>
2025-06-26 16:26:18 +0530

    Update variables.tf

--- modules/resourcegroup/variables.tf
+++ modules/resourcegroup/variables.tf
@@ -1,9 +1,3 @@
-variable "resource_group_name" {
-  description = "Name of the Azure Resource Group"
-  type        = string
-  default     = "testing-5"
-}
-
 variable "location" {
   description = "Azure region for the Resource Group"
   type        = string
