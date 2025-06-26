8cec75c1251d887b46734f75c72f18f05c240f15
shubhi0302 <163977746+shubhi0302@users.noreply.github.com>
2025-06-26 16:25:55 +0530

    Update main.tf

--- modules/resourcegroup/main.tf
+++ modules/resourcegroup/main.tf
@@ -1,23 +1,61 @@
+variable "resource_group_names" {
+  type = list(string)
+  default = [
+    "qf-resourcegroup",
+    "qf-resourcegroup-1",
+    "qf-resourcegroup-2",
+    "qf-resourcegroup-3",
+    "qf-test-rg",
+    "qf-test-rg-1",
+    "qf-test-rg-2",
+    "qf-test-rg-3",
+    "qf-test-rg-4",
+    "qf-test-rg-5",
+    "qf-test-rg-6",
+    "qf-test-rg-7",
+    "qf-test-rg-8",
+    "qf-test-rg-9",
+    "qf-test-rg-10",
+    "qf-test-rg-11",
+    "qf-test-rg-13",
+    "qf-test-rg-15",
+    "qf-test-rg-21",
+    "qf-test-rg-github",
+    "qualityframework-demo-rg" 
+  ]
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  for_each = toset(var.resource_group_names)
+
+  name     = each.key
   location = var.location
 }
 
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+output "all_resource_group_names" {
+  value = [for rg in azurerm_resource_group.rg : rg.name]
 }
 
-output "resource_group_location" {
-  value = azurerm_resource_group.rg.location
-}
+# Correct storage account-resource group mapping from your screenshot
+resource "azurerm_storage_account" "sa" {
+  for_each = {
+    qualityframeworksa0302 = "qf-resourcegroup"
+    qualityframeworksa0303 = "qf-resourcegroup-1"
+    qualityframeworksa0305 = "qf-resourcegroup-3"
+    qualityframeworksa12   = "qualityframework-demo-rg"
+  }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "qualityframeworksa15" # must be globally unique and 3-24 lowercase letters/numbers
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  name                     = each.key
+  resource_group_name      = each.value
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  min_tls_version          = "TLS1_2" # 🔐 Secure TLS config
+  min_tls_version          = "TLS1_2"
 
   tags = {
     environment = "dev"
@@ -25,6 +63,6 @@ resource "azurerm_storage_account" "storage" {
   }
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.storage.name
+output "all_storage_account_names" {
+  value = [for sa in azurerm_storage_account.sa : sa.name]
 }
